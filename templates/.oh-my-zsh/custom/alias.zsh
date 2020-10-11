2f76857d1412816a88103fb698e7be2df6ec4df6
Grégory Sanchez <gregory.sanchez@icloud.com>
2020-10-11 22:43:16 +0200

    chore(): remove uneeded

--- templates/.oh-my-zsh/custom/alias.zsh
+++
@@ -1,8 +0,0 @@
-{{ ansible_managed | comment }}
-
-alias cd..="cd .."
-
-alias ls='ls --color=auto'
-alias ll='ls -l --color=auto'
-alias rm='rm -i'
-alias mv='mv -i'
--- templates/.oh-my-zsh/custom/env.zsh
+++
@@ -1,3 +0,0 @@
-{{ ansible_managed | comment }}
-
-export PATH=$HOME/.local/bin:$PATH
